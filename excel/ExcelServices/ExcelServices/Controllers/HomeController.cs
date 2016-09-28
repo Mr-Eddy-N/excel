db910ce9ff29ef6e178462a396d7907d34bfb235
edgar7386 <edgarreyesrojas@gmail.com>
2016-09-27 22:11:20 -0500

    Model DATA lista

--- excel/ExcelServices/ExcelServices/Controllers/HomeController.cs
+++ excel/ExcelServices/ExcelServices/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using System.Reflection;
 using DocumentFormat.OpenXml;
 using ClosedXML.Excel;
+using ExcelServices.Models;
 
 namespace ExcelServices.Controllers
 {
@@ -45,9 +46,30 @@ namespace ExcelServices.Controllers
         [HttpGet]
         public virtual ActionResult DownloadExcel()
         {
+            
+            string filePath = Path.Combine(Server.MapPath("~/MyFiles/ex.xlsx"));
+            var workbook = new XLWorkbook();
+            var ws = workbook.Worksheets.Add("Column Cells");
+
+            var columnFromWorksheet = ws.Column(1);
+            columnFromWorksheet.Cell(1).Style.Fill.BackgroundColor = XLColor.Red;
+            columnFromWorksheet.Cells("2").Style.Fill.BackgroundColor = XLColor.Blue;
+            columnFromWorksheet.Cells("3,5:6").Style.Fill.BackgroundColor = XLColor.Red;
+            columnFromWorksheet.Cells(8, 9).Style.Fill.BackgroundColor = XLColor.Blue;
+
+            var columnFromRange = ws.Range("B1:B9").FirstColumn();
+
+            columnFromRange.Cell(1).Style.Fill.BackgroundColor = XLColor.Red;
+            columnFromRange.Cells("2").Style.Fill.BackgroundColor = XLColor.Blue;
+            columnFromRange.Cells("3,5:6").Style.Fill.BackgroundColor = XLColor.Red;
+            columnFromRange.Cells(8, 9).Style.Fill.BackgroundColor = XLColor.Blue;
+
+            workbook.SaveAs(filePath);
             string file ="ex.xlsx";
             string fullPath = Path.Combine(Server.MapPath("~/MyFiles"), file);
             return File(fullPath, "application/vnd.ms-excel", file);
+            //return File(workbook,"application/vnd.ms-excel","reportesotee.xlsx");
         }
+
     }
 }
---
+++ excel/ExcelServices/ExcelServices/Models/DATAEX.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExcelServices.Models
+{
+    public class DATAEX
+    {
+        string listName { get; set; }
+        string fileName { get; set; }
+        string site { get; set; }
+    }
+}

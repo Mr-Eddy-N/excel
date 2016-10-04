43d59a4b8b082e88e52cc02500bd70108a4629d0
edgar7386 <edgarreyesrojas@gmail.com>
2016-10-03 22:24:01 -0500

    da

---
+++ Movies/Movies/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Movies.Controllers
+{
+    public class MoviesController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Demo(int id)
+        {
+            List<int> l = new List<int>() { 1, 2, 3 };
+            return Ok(l.Where(x => x == 1).ToList());
+            // return Ok(new List<int>() { 1, 2, 3 });
+        }
+    }
+}
---
+++ Movies/Movies/Scripts/appMovies.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module('appMovies',[])
+var Controllers = {};
+    Controllers.newMovie = function ($scope,$http) {
+    $scope.save = function () {
+        console.log($scope.Movie);
+        $scope.result = JSON.stringify($scope.Movie);
+    }
+}
+app.controller(Controllers);
